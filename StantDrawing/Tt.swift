12fdfa6b86211dad42382bb290f918728eb0630a
Rachid Calazans <rachidcalazans@gmail.com>
2017-07-02 16:33:54 -0300

    Testing podspec
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

---
+++ StantDrawing/Tt.swift
@@ -0,0 +1,14 @@
+//
+//  Tt.swift
+//  StantDrawing
+//
+//  Created by Rachid Calazans on 02/07/17.
+//  Copyright © 2017 Stant. All rights reserved.
+//
+
+import Foundation
+public class Tt {
+    class func tt() {
+        print("Hello")
+    }
+}
