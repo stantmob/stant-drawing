7bdfc933b8a077cf7ab50efda89a59a2a42de28a
Rachid Calazans <rachidcalazans@gmail.com>
2017-07-02 15:11:05 -0300

    Initial commit
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

---
+++ StantDrawing/StantDrawing.h
@@ -0,0 +1,19 @@
+//
+//  StantDrawing.h
+//  StantDrawing
+//
+//  Created by Rachid Calazans on 02/07/17.
+//  Copyright © 2017 Stant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StantDrawing.
+FOUNDATION_EXPORT double StantDrawingVersionNumber;
+
+//! Project version string for StantDrawing.
+FOUNDATION_EXPORT const unsigned char StantDrawingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StantDrawing/PublicHeader.h>
+
+
