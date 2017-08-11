d0df266df49a7ed160face88f92911ffe2297e1f
Rachid Calazans <rachidcalazans@gmail.com>
2017-08-11 18:54:55 -0300

    Update BrushToolView
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- StantDrawing/BrushTool/BrushToolView.swift
+++ StantDrawing/BrushTool/BrushToolView.swift
@@ -12,8 +12,14 @@ import Foundation
 public class BrushToolView: UIView {
     var delegate: BrushToolContract?
     
-    private let btnWidth  = 60
-    private let btnHeight = 60
+    private let bundle = Bundle(for: BrushToolView.self)
+    
+    private let btnWidth  = 45
+    private let btnHeight = 45
+    
+    private let groupToolsView = UIView()
+    private let separatorView  = UIView()
+    private let groupEndView   = UIView()
     
     private let moveBtn   = UIButton()
     private let brushBtn  = UIButton()
@@ -23,89 +29,175 @@ public class BrushToolView: UIView {
     private let saveBtn   = UIButton()
     private let cancelBtn = UIButton()
     
-//    public class func instanceFromNib() -> BrushToolView {
-//        let podBundle = Bundle(for: BrushToolView.self)
-//        
-//        let bundleURL = podBundle.url(forResource: "StantDrawing", withExtension: "bundle")
-//        let bundle = Bundle(url: bundleURL!)
-//        return UINib(nibName: "BrushToolView", bundle: bundle)
-//        .instantiate(withOwner: BrushToolView.init(), options: nil)[0] as! BrushToolView
-//    }
-    
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     init() {
         let screenSize = UIScreen.main.bounds
-        let frame      = CGRect(x: 0, y: 0, width: 60, height: screenSize.height)
+        let frame      = CGRect(x: 10, y: 0, width: 85, height: screenSize.height)
 
         super.init(frame: frame)
-
-        self.backgroundColor = UIColor.gray
         
-        configureButtons()
+        configureLayout()
+        addBtnListeners()
+        
+        setBtnAsClicked(button: moveBtn)
     }
     
-    private func nextBtnFrame(referencedBtn: UIButton) -> CGRect {
-        return CGRect(x: 0, y: Int(referencedBtn.frame.origin.y + 75), width: btnWidth, height: btnHeight)
+    private func configureLayout() {
+        configureBtnPositions()
+        configureBtnIcons()
+        setAllBtnsAsNotClicked()
+
+        configureGroupViews()
+        
+        addSubViews()
     }
     
-    private func configureButtons() {
-        moveBtn.frame   = CGRect(x: 0, y: 30, width: btnWidth, height: btnHeight)
+    private func configureBtnPositions() {
+        let x = (Int(self.frame.width) - btnWidth) / 2
+        let frame = CGRect(x: x, y: 20, width: btnWidth, height: btnHeight)
+        
+        moveBtn.frame   = frame
         brushBtn.frame  = nextBtnFrame(referencedBtn: moveBtn)
         undoBtn.frame   = nextBtnFrame(referencedBtn: brushBtn)
         redoBtn.frame   = nextBtnFrame(referencedBtn: undoBtn)
         eraseBtn.frame  = nextBtnFrame(referencedBtn: redoBtn)
-        saveBtn.frame   = nextBtnFrame(referencedBtn: eraseBtn)
+        
+        saveBtn.frame   = frame
         cancelBtn.frame = nextBtnFrame(referencedBtn: saveBtn)
         
-        let bundle = Bundle(for: BrushToolView.self)
+        setImageEdgeInsets(btn: moveBtn)
+        setImageEdgeInsets(btn: brushBtn)
+        setImageEdgeInsets(btn: undoBtn)
+        setImageEdgeInsets(btn: redoBtn)
+        setImageEdgeInsets(btn: eraseBtn)
+        setImageEdgeInsets(btn: saveBtn)
+        setImageEdgeInsets(btn: cancelBtn)
+    }
+    
+    private func nextBtnFrame(referencedBtn: UIButton) -> CGRect {
+        let x = (Int(self.frame.width) - btnWidth) / 2
+        return CGRect(x: x, y: Int(referencedBtn.frame.origin.y + 75), width: btnWidth, height: btnHeight)
+    }
+    
+    private func setImageEdgeInsets(btn: UIButton) {
+        btn.imageEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    }
+    
+    private func configureBtnIcons() {
+        setBtnImage(btn: moveBtn,   imageName: "move")
+        setBtnImage(btn: brushBtn,  imageName: "paint-brush")
+        setBtnImage(btn: undoBtn,   imageName: "undo")
+        setBtnImage(btn: redoBtn,   imageName: "redo")
+        setBtnImage(btn: eraseBtn,  imageName: "eraser")
+        setBtnImage(btn: saveBtn,   imageName: "save")
+        setBtnImage(btn: cancelBtn, imageName: "cancel")
+    }
+
+    private func setBtnImage(btn: UIButton, imageName: String) {
+        let image = UIImage.init(named: imageName, in: bundle, compatibleWith: nil)
+        btn.setImage(image, for: .normal)
+    }
+    
+    private func addBtnListeners() {
+        addBtnListener(moveBtn,   action: #selector(self.moveCanvas))
+        addBtnListener(brushBtn,  action: #selector(self.drawOnCanvas))
+        addBtnListener(undoBtn,   action: #selector(self.undo))
+        addBtnListener(redoBtn,   action: #selector(self.redo))
+        addBtnListener(eraseBtn,  action: #selector(self.erase))
+        addBtnListener(saveBtn,   action: #selector(self.save))
+        addBtnListener(cancelBtn, action: #selector(self.cancel))
+    }
+    
+    private func addBtnListener(_ btn: UIButton, action: Selector) {
+        btn.addTarget(self, action: action, for: .touchUpInside)
+    }
+    
+    private func configureGroupViews() {
+        let origin = CGPoint(x: 0, y: 100)
+        let size   = CGSize(width: self.frame.size.width, height: eraseBtn.frame.origin.y + eraseBtn.frame.height + 20)
+        let frame = CGRect.init(origin: origin, size: size)
+        groupToolsView.frame = frame
         
-        moveBtn.setTitle("Move", for: .normal)
-        let moveImg = UIImage.init(named: "move", in: bundle, compatibleWith: nil)
-        moveBtn.setImage(moveImg, for: .normal)
-//        moveBtn.setImage(UIImage.init(named: "move"), for: .normal)
+        groupToolsView.backgroundColor = UIColor(hex: "F0F0F0")
         
-        brushBtn.setTitle("Brush", for: .normal)
-        undoBtn.setTitle("Undo", for: .normal)
-        redoBtn.setTitle("Redo", for: .normal)
-        eraseBtn.setTitle("Erase", for: .normal)
-        saveBtn.setTitle("Save", for: .normal)
-        cancelBtn.setTitle("Cancel", for: .normal)
+        let origin2 = CGPoint(x: 20, y: groupToolsView.frame.origin.y + groupToolsView.frame.height + 10)
+        let size2 = CGSize(width: self.frame.width - 40, height: 1)
+        let frame2 = CGRect.init(origin: origin2, size: size2)
+        separatorView.frame = frame2
         
-        moveBtn.backgroundColor = UIColor.blue
-        eraseBtn.backgroundColor = UIColor.orange
-        cancelBtn.backgroundColor = UIColor.purple
+        separatorView.backgroundColor = UIColor.black
+        separatorView.alpha = 0.2
         
-        moveBtn.addTarget(self, action: #selector(self.moveCanvas), for: .touchUpInside)
-        brushBtn.addTarget(self, action: #selector(self.drawOnCanvas), for: .touchUpInside)
-        undoBtn.addTarget(self, action: #selector(self.undo), for: .touchUpInside)
-        redoBtn.addTarget(self, action: #selector(self.redo), for: .touchUpInside)
-        eraseBtn.addTarget(self, action: #selector(self.erase), for: .touchUpInside)
-        saveBtn.addTarget(self, action: #selector(self.save), for: .touchUpInside)
-        cancelBtn.addTarget(self, action: #selector(self.cancel), for: .touchUpInside)
+        let origin3 = CGPoint(x: 0, y: frame2.height + frame2.origin.y + 10)
+        let size3 = CGSize(width: self.frame.size.width, height: cancelBtn.frame.origin.y + cancelBtn.frame.height + 20)
+        let frame3 = CGRect.init(origin: origin3, size: size3)
+        groupEndView.frame = frame3
         
-        self.addSubview(moveBtn)
-        self.addSubview(brushBtn)
-        self.addSubview(undoBtn)
-        self.addSubview(redoBtn)
-        self.addSubview(eraseBtn)
-        self.addSubview(saveBtn)
-        self.addSubview(cancelBtn)
+        groupEndView.backgroundColor = UIColor(hex: "F0F0F0")
+        
+        groupToolsView.layer.cornerRadius = groupToolsView.frame.width / 2
+        
+        groupEndView.layer.cornerRadius = groupEndView.frame.width / 2
+
+    }
+
+    private func addSubViews() {
+        self.addSubview(groupToolsView)
+        self.addSubview(separatorView)
+        self.addSubview(groupEndView)
+        
+        groupToolsView.addSubview(moveBtn)
+        groupToolsView.addSubview(brushBtn)
+        groupToolsView.addSubview(undoBtn)
+        groupToolsView.addSubview(redoBtn)
+        groupToolsView.addSubview(eraseBtn)
+        
+        groupEndView.addSubview(saveBtn)
+        groupEndView.addSubview(cancelBtn)
+    }
+    
+    private func setBtnAsClicked(button: UIButton) {
+        button.alpha = 1
+        button.layer.cornerRadius = button.frame.height / 2
+        button.backgroundColor = UIColor.white
+    }
+    
+    private func setAllBtnsAsNotClicked() {
+        setBtnAsNotClicked(button: self.moveBtn)
+        setBtnAsNotClicked(button: self.eraseBtn)
+        setBtnAsNotClicked(button: self.brushBtn)
+        setBtnAsNotClicked(button: self.undoBtn)
+        setBtnAsNotClicked(button: self.redoBtn)
+    }
+    
+    private func setBtnAsNotClicked(button: UIButton) {
+        button.alpha = 0.4
+        button.backgroundColor = UIColor.clear
     }
     
     // MARK: IBActions
     
     func moveCanvas(_ sender: Any) {
+        setAllBtnsAsNotClicked()
+        setBtnAsClicked(button: self.moveBtn)
+        
         self.delegate?.moveCanvas()
     }
     
     func erase(_ sender: Any) {
+        setAllBtnsAsNotClicked()
+        setBtnAsClicked(button: self.eraseBtn)
+        
         self.delegate?.erase()
     }
     
     func drawOnCanvas(_ sender: Any) {
+        setAllBtnsAsNotClicked()
+        setBtnAsClicked(button: self.brushBtn)
+        
         delegate?.draw()
     }
     
@@ -135,6 +227,29 @@ public class BrushToolView: UIView {
             self.delegate?.cancel()
         }))
         UIApplication.shared.keyWindow?.rootViewController!.topMostViewController().present(alert, animated: true, completion: nil)
+    }
+}
+
+extension UIColor {
+    convenience init(hex string: String) {
+        var hex = string.hasPrefix("#")
+            ? String(string.characters.dropFirst())
+            : string
+        guard hex.characters.count == 3 || hex.characters.count == 6
+            else {
+                self.init(white: 1.0, alpha: 0.0)
+                return
+        }
+        if hex.characters.count == 3 {
+            for (index, char) in hex.characters.enumerated() {
+                hex.insert(char, at: hex.index(hex.startIndex, offsetBy: index * 2))
+            }
+        }
+        
+        self.init(
+            red:   CGFloat((Int(hex, radix: 16)! >> 16) & 0xFF) / 255.0,
+            green: CGFloat((Int(hex, radix: 16)! >> 8) & 0xFF) / 255.0,
+            blue:  CGFloat((Int(hex, radix: 16)!) & 0xFF) / 255.0, alpha: 1.0)
     }
 }
 
